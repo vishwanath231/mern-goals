27b0cf0af6701f66457b19307fb4ffd3f6e76099
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-22 21:40:31 +0530

    Server app

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,27 @@ import morgan from "morgan";
 import colors from 'colors';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
+dotenv.config()
+
+// MongoDB connection
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.use(cors());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan("dev"))
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT, 
+    console.log(`server running on ${process.env.NODE_ENV} mode port ${process.env.PORT}`.yellow.underline)
+);
