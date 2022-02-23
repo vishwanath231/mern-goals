51708515b6e5351bc14fd7be3cf61950a6b53f50
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-24 01:05:40 +0530

    Goal controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,60 @@
+import Goal from '../models/goalModel.js';
+import asyncHandler from 'express-async-handler';
 
-const getGoals = (req, res) => {
-    res.send("goals");
-} 
+
+/**
+ * @router  POST api/goals
+ * @desc    create a new goal
+ * @access  Public
+ */
+
+const postGoal = asyncHandler(async (req, res) => {
+
+    res.send("New goal");
+});
+
+
+/**
+ * @router  GET api/goals
+ * @desc    Get the all goals
+ * @access  Public
+ */
+
+const getGoals = asyncHandler(async (req, res) => {
+
+    res.send("Get goals");
+});
+
+
+
+/**
+ * @router  PUT api/goals/:id
+ * @desc    Update a goal
+ * @access  Public
+ */
+const updateGoal = asyncHandler(async (req, res) => {
+
+    res.send("Update Goal");
+});
+
+
+
+/**
+ * @router  DELETE api/goals/:id
+ * @desc    Delete a goal
+ * @access  Public
+ */
+
+const deleteGoal = asyncHandler(async (req, res) => {
+
+    res.send("Delete Goal");
+});
+
+
 
 export {
+    postGoal,
     getGoals,
+    updateGoal,
+    deleteGoal
 }
