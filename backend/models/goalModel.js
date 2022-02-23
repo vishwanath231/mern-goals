d4265da6e1a5d1550dd89c7c5e1ea10c17fb7ff6
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-24 01:05:14 +0530

    Goal model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const goalSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     goal: {
         type: String,
         required: true
@@ -14,6 +19,6 @@ const goalSchema = new Schema({
     timestamps: true
 })
 
-const Goal = mongoose.model('users', goalSchema);
+const Goal = mongoose.model('goals', goalSchema);
 
 export default Goal;
