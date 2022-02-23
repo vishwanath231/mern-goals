4af0da64ce99b340c60b1dd7d7e6916ef34aec30
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-23 23:15:56 +0530

    Error middleware

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -9,7 +9,7 @@ const errorHandler = (err, req, res, next) => {
         msg: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
     })
-
 }
+
 
 export default errorHandler;
