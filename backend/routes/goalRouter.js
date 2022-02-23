4794f4311ce6b2a2723b506565d5a8b273658e76
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-24 01:04:02 +0530

    Goal router

--- backend/routes/goalRouter.js
+++ backend/routes/goalRouter.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 const router = Router();
-import { getGoals } from '../controllers/goalController.js';
+import { postGoal, getGoals, updateGoal, deleteGoal } from '../controllers/goalController.js';
+import protect from '../middlewares/authMiddleware.js';
 
-router
-.route('/')
-.get(getGoals);
+router.route('/').get(protect, getGoals).post(protect, postGoal);
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal);
 
 export default router;
