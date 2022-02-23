0f40009221fc7b21b316e67b5509be6a5bd195da
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-23 23:21:50 +0530

    Server app

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import goalRouter from './routes/goalRouter.js';
 import userRouter from './routes/userRouter.js';
 import errorHandler from './middlewares/errorMiddleware.js'
 
-
+// env
 dotenv.config()
 
 // MongoDB connection
@@ -16,22 +16,29 @@ connectDB();
 
 const app = express();
 
+// Express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+// cors => Cross Origin Resource Sharing
 app.use(cors());
 
+// morgan for development mode
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan("dev"))
 }
 
+// router
 app.use('/api/users', userRouter);
 app.use('/api/goals', goalRouter);
 
+// Error handler
 app.use(errorHandler);
 
+// Port
 const PORT = process.env.PORT || 5000;
 
+// App listen
 app.listen(
     PORT, 
     console.log(`server running on ${process.env.NODE_ENV} mode port ${process.env.PORT}`.yellow.underline)
