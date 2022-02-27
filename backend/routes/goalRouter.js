beea18637bcd67b248beace4be1fbf5eadecc8f7
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-27 22:49:01 +0530

    Goal router

--- backend/routes/goalRouter.js
+++ backend/routes/goalRouter.js
@@ -5,5 +5,4 @@ import protect from '../middlewares/authMiddleware.js';
 
 router.route('/').get(protect, getGoals).post(protect, postGoal);
 router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal);
-
 export default router;
