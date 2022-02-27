f2b33cb1381c6a6ad548c26ac80f64ad770a6713
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-27 22:52:20 +0530

    App react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React,{ useEffect } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import HomeScreen from './screen/HomeScreen';
+import LoginScreen from './screen/LoginScreen';
+import RegisterScreen from './screen/RegisterScreen';
+import { userLoaded } from './redux/actions/authAction';
+import store from './redux/store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+
+    useEffect(() => {
+        
+        store.dispatch(userLoaded());
+        
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<HomeScreen />} />
+                <Route path='/login' element={<LoginScreen />} />
+                <Route path='/register' element={<RegisterScreen />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
