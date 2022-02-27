2eaae3c81b26b59ca2ed35db77406108b125df44
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-27 23:05:47 +0530

    Types action

---
+++ frontend/src/redux/actions/types.js
@@ -0,0 +1,31 @@
+
+// USER LOADING & LOADED
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+// LOGIN
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+
+// REGISTER
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+
+// LOGOUT 
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+
+// ERRORS
+export const AUTH_ERROR = "AUTH_ERROR";
+export const GET_ERROR = "GET_ERROR";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+// GOAL
+export const GOAL_LOADING = "GOAL_LOADING";
+export const ADD_GOAL = "ADD_GOAL";
+export const GET_GOALS = "GET_GOALS";
+export const DELETE_GOAL = "DELETE_GOAL";
+export const GOAL_CHECK = "GOAL_CHECK";
+
+// SUCCESS
+export const SUCCESS_MSG = "SUCCESS_MSG";
+export const SUCCESS_MSG_CLEAR = "SUCCESS_MSG_CLEAR";
